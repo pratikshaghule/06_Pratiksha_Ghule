43a8494631fa4be4350985213747698b27395233
pratikshaghule <65436979+pratikshaghule@users.noreply.github.com>
2020-12-31 22:35:35 +0530

    Add files via upload

---
+++ P2-CF&L/Mylogger.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mylogger;
+
+/**
+ *
+ * @author Satya
+ */
+import java.io.*;
+import java.util.logging.*;
+public class Mylogger {
+    public static void main(String args[])
+    {
+        Logger l=Logger.getLogger(Mylogger.class.getName());
+        FileHandler fh;
+        try
+        {
+            fh=new FileHandler("c:/mylogfile.log",true);
+            l.addHandler(fh);
+            l.setLevel(Level.ALL);
+            SimpleFormatter sf=new SimpleFormatter();
+            fh.setFormatter(sf);
+            l.info("My first log");
+        }
+        catch(SecurityException e)
+        {
+            e.printStackTrace();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+        l.info("Hi How r u?");
+    }
+}
